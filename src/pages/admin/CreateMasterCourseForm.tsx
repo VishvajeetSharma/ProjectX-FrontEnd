9ca27ec97abd70e70edf7ed85c50795757a8ee0e
Vishvajeet Sharma <vsharma6705@gmail.com>
2026-03-23 19:30:19 +0530

    validation on plan, & course

--- src/pages/admin/CreateMasterCourseForm.tsx
+++ src/pages/admin/CreateMasterCourseForm.tsx
@@ -1,19 +1,64 @@
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
+
+// Schema
+const schema = yup.object().shape({
+  name: yup
+    .string()
+    .required("Course name is required")
+    .matches(/\S/, "Only spaces not allowed")
+    .min(3, "Minimum 3 characters")
+    .max(50, "Maximum 50 characters"),
+
+  desc: yup
+    .string()
+    .nullable()
+    .notRequired()
+    .min(5, "Minimum 5 characters")
+    .max(255, "Maximum 255 characters"),
+
+   status: yup
+      .number()
+      .oneOf([0, 1], "Invalid status")
+      .default(1),
+});
+
 const CreateMasterCourseForm = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(schema),
+    defaultValues: {
+      status: 1,
+    },
+  });
+
+  const onSubmit = (data: any) => {
+    console.log("Course Data:", data);
+  };
+
   return (
     <div className="row my-bg-dark p-5">
       <div className="col-sm-8 mx-auto">
         <div className="card my-second-bg-dark text-white p-4 border-0 rounded-4 shadow-lg">
           <h2 className="fw-bold mb-4 text-center">Create Master Course</h2>
 
-          <form>
+          <form onSubmit={handleSubmit(onSubmit)}>
             {/* Course Name */}
             <div className="mb-3">
               <label className="form-label">Master Course Name *</label>
               <input
                 type="text"
+                {...register("name")}
                 className="form-control border-0"
                 placeholder="Enter course name"
               />
+              {errors.name && (
+                <small className="text-danger">{errors.name.message}</small>
+              )}
             </div>
 
             {/* Description */}
@@ -21,18 +66,30 @@ const CreateMasterCourseForm = () => {
               <label className="form-label">Description</label>
               <input
                 type="text"
+                {...register("desc")}
                 className="form-control border-0"
                 placeholder="Enter course description"
               />
+              {errors.desc && (
+                <small className="text-danger">{errors.desc.message}</small>
+              )}
             </div>
 
             {/* Status */}
             <div className="mb-3">
               <label className="form-label">Status *</label>
-              <select className="form-control border-0">
+              <select
+                {...register("status")}
+                className="form-control border-0"
+              >
                 <option value={1}>Active</option>
                 <option value={0}>Inactive</option>
               </select>
+              {errors.status && (
+                <small className="text-danger">
+                  {errors.status.message}
+                </small>
+              )}
             </div>
 
             {/* Button */}
--- src/pages/admin/MasterPlanForm.tsx
+++ src/pages/admin/MasterPlanForm.tsx
@@ -1,70 +1,128 @@
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
+
+// Schema
+const schema = yup.object().shape({
+  name: yup
+    .string()
+    .required("Master Plan Name is required")
+    .matches(/\S/, "Only spaces not allowed")
+    .min(3, "Minimum 3 characters")
+    .max(50, "Maximum 50 characters"),
+
+  desc: yup
+    .string()
+    .required("Description is required")
+    .min(5, "Minimum 5 characters")
+    .max(255, "Maximum 255 characters"),
+
+  credit: yup
+    .number()
+    .typeError("Credit must be a number")
+    .required("Credit is required")
+    .positive("Must be a positive number")
+    .integer("Must be an integer"),
+
+  status: yup
+    .number()
+    .oneOf([0, 1], "Invalid status")
+    .default(1),
+});
 
 const MasterPlanForm = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(schema),
+    defaultValues: {
+      status: 1,
+    },
+  });
+
+  const onSubmit = (data: any) => {
+    console.log("Master Plan Data:", data);
+  };
+
   return (
     <div className="row my-bg-dark p-5">
-      <div className="col-sm-8 mx-auto">
+      <div className="col-sm-10 mx-auto">
         <div className="card my-second-bg-dark text-white p-4 border-0 rounded-4 shadow-lg">
           <h2 className="fw-bold mb-4 text-center">Create Master Plan</h2>
 
-          <form >
+          <form onSubmit={handleSubmit(onSubmit)}>
             {/* Name */}
             <div className="mb-3">
               <label className="form-label">Master Plan Name *</label>
               <input
                 type="text"
+                {...register("name")}
                 className="form-control border-0"
                 placeholder="Enter your name"
               />
-
+              {errors.name && (
+                <small className="text-danger">{errors.name.message}</small>
+              )}
             </div>
 
-            {/* Email */}
+            {/* Description */}
             <div className="mb-3">
-              <label className="form-label">Email *</label>
-              <input
-                type="email"
-                className="form-control border-0"
-                placeholder="Enter your email"
-              />
-
-            </div>
-
-            {/* Mobile */}
-            <div className="mb-3">
-              <label className="form-label">Mobile number *</label>
+              <label className="form-label">Description *</label>
               <input
                 type="text"
+                {...register("desc")}
                 className="form-control border-0"
-                placeholder="Enter your number"
+                placeholder="Enter description"
               />
-
+              {errors.desc && (
+                <small className="text-danger">{errors.desc.message}</small>
+              )}
             </div>
 
-            {/* Password */}
+            {/* Credit */}
             <div className="mb-3">
-              <label className="form-label">Password *</label>
+              <label className="form-label">Credit *</label>
               <input
-                type="password"
-
+                type="text"
+                {...register("credit")}
                 className="form-control border-0"
-                placeholder="Enter your password"
+                placeholder="Enter credit"
               />
+              {errors.credit && (
+                <small className="text-danger">
+                  {errors.credit.message}
+                </small>
+              )}
+            </div>
 
+           {/* Status */}
+            <div className="mb-3">
+              <label className="form-label">Status *</label>
+              <select
+                {...register("status")}
+                className="form-control border-0"
+              >
+                <option value={1}>Active</option>
+                <option value={0}>Inactive</option>
+              </select>
+              {errors.status && (
+                <small className="text-danger">
+                  {errors.status.message}
+                </small>
+              )}
             </div>
 
             {/* Button */}
             <button className="btn my-btn w-100 py-2 mt-4 fw-bold">
-              Submit
+              Create
             </button>
           </form>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default MasterPlanForm
-
-
-
-
+export default MasterPlanForm;
