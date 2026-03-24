52b894579659cf2b7cd7d6b99a568bebdebe0955
Vishvajeet Sharma <vsharma6705@gmail.com>
2026-03-24 08:51:46 +0530

    plan form

--- src/pages/admin/MasterPlanForm.tsx
+++ src/pages/admin/MasterPlanForm.tsx
@@ -3,6 +3,9 @@ import { yupResolver } from "@hookform/resolvers/yup";
 import * as yup from "yup";
 
 // Schema
+
+
+
 const schema = yup.object().shape({
   name: yup
     .string()
@@ -23,6 +26,32 @@ const schema = yup.object().shape({
     .required("Credit is required")
     .positive("Must be a positive number")
     .integer("Must be an integer"),
+
+  price: yup
+    .number()
+    .typeError("Price must be a number")
+    .required("Price is required")
+    .positive("Must be a positive number")
+    .integer("Must be an integer"),
+
+  offer: yup
+    .number()
+    .typeError("Offer must be a number")
+    .required("Offer is required")
+    .positive("Must be a positive number")
+    .integer("Must be an integer"),
+
+  duration: yup
+    .number()
+    .typeError("Duration must be a number")
+    .required("Duration is required")
+    .positive("Must be a positive number")
+    .integer("Must be an integer"),
+
+  is_rec: yup
+    .number()
+    .oneOf([0, 1], "Invalid recomendation")
+    .default(1),
 
   status: yup
     .number()
@@ -53,67 +82,154 @@ const MasterPlanForm = () => {
           <h2 className="fw-bold mb-4 text-center">Create Master Plan</h2>
 
           <form onSubmit={handleSubmit(onSubmit)}>
-            {/* Name */}
-            <div className="mb-3">
-              <label className="form-label">Master Plan Name *</label>
-              <input
-                type="text"
-                {...register("name")}
-                className="form-control border-0"
-                placeholder="Enter your name"
-              />
-              {errors.name && (
-                <small className="text-danger">{errors.name.message}</small>
-              )}
+
+            <div className="row">
+              <div className="col-sm-6">
+                {/* Name */}
+                <div className="mb-3">
+                  <label className="form-label">Master Plan Name *</label>
+                  <input
+                    type="text"
+                    {...register("name")}
+                    className="form-control border-0"
+                    placeholder="Enter your name"
+                  />
+                  {errors.name && (
+                    <small className="text-danger">{errors.name.message}</small>
+                  )}
+                </div>
+              </div>
+              <div className="col-sm-6">
+                {/* Description */}
+                <div className="mb-3">
+                  <label className="form-label">Description *</label>
+                  <input
+                    type="text"
+                    {...register("desc")}
+                    className="form-control border-0"
+                    placeholder="Enter description"
+                  />
+                  {errors.desc && (
+                    <small className="text-danger">{errors.desc.message}</small>
+                  )}
+                </div>
+              </div>
             </div>
 
-            {/* Description */}
-            <div className="mb-3">
-              <label className="form-label">Description *</label>
-              <input
-                type="text"
-                {...register("desc")}
-                className="form-control border-0"
-                placeholder="Enter description"
-              />
-              {errors.desc && (
-                <small className="text-danger">{errors.desc.message}</small>
-              )}
+            <div className="row">
+              <div className="col-sm-6">
+                {/* Credit */}
+                <div className="mb-3">
+                  <label className="form-label">Credit *</label>
+                  <input
+                    type="text"
+                    {...register("credit")}
+                    className="form-control border-0"
+                    placeholder="Enter credit"
+                  />
+                  {errors.credit && (
+                    <small className="text-danger">
+                      {errors.credit.message}
+                    </small>
+                  )}
+                </div>
+              </div>
+              <div className="col-sm-6">
+                {/* Price */}
+                <div className="mb-3">
+                  <label className="form-label">Price *</label>
+                  <input
+                    type="text"
+                    {...register("price")}
+                    className="form-control border-0"
+                    placeholder="Enter price"
+                  />
+                  {errors.price && (
+                    <small className="text-danger">
+                      {errors.price.message}
+                    </small>
+                  )}
+                </div>
+              </div>
             </div>
 
-            {/* Credit */}
-            <div className="mb-3">
-              <label className="form-label">Credit *</label>
-              <input
-                type="text"
-                {...register("credit")}
-                className="form-control border-0"
-                placeholder="Enter credit"
-              />
-              {errors.credit && (
-                <small className="text-danger">
-                  {errors.credit.message}
-                </small>
-              )}
+            <div className="row">
+              <div className="col-sm-6">
+                {/* Offer */}
+                <div className="mb-3">
+                  <label className="form-label">Offer *</label>
+                  <input
+                    type="text"
+                    {...register("offer")}
+                    className="form-control border-0"
+                    placeholder="Enter offer"
+                  />
+                  {errors.offer && (
+                    <small className="text-danger">
+                      {errors.offer.message}
+                    </small>
+                  )}
+                </div>
+              </div>
+              <div className="col-sm-6">
+                {/* Duration */}
+                <div className="mb-3">
+                  <label className="form-label">Duration *</label>
+                  <input
+                    type="text"
+                    {...register("duration")}
+                    className="form-control border-0"
+                    placeholder="Enter duration"
+                  />
+                  {errors.duration && (
+                    <small className="text-danger">
+                      {errors.duration.message}
+                    </small>
+                  )}
+                </div>
+              </div>
             </div>
 
-           {/* Status */}
-            <div className="mb-3">
-              <label className="form-label">Status *</label>
-              <select
-                {...register("status")}
-                className="form-control border-0"
-              >
-                <option value={1}>Active</option>
-                <option value={0}>Inactive</option>
-              </select>
-              {errors.status && (
-                <small className="text-danger">
-                  {errors.status.message}
-                </small>
-              )}
-            </div>
+            <div className="row">
+              <div className="col-sm-6">
+                {/* Is Recommended */}
+                <div className="mb-3">
+                  <label className="form-label">Recommended *</label>
+                  <select
+                    {...register("is_rec")}
+                    className="form-control border-0"
+                  >
+                    <option value={0}>False</option>
+                    <option value={1}>True</option>
+                  </select>
+                  {errors.is_rec && (
+                    <small className="text-danger">
+                      {errors.is_rec.message}
+                    </small>
+                  )}
+                </div>
+              </div>
+              <div className="col-sm-6">
 
+                {/* Status */}
+                <div className="mb-3">
+                  <label className="form-label">Status *</label>
+                  <select
+                    {...register("status")}
+                    className="form-control border-0"
+                  >
+                    <option value={1}>Active</option>
+                    <option value={0}>Inactive</option>
+                  </select>
+                  {errors.status && (
+                    <small className="text-danger">
+                      {errors.status.message}
+                    </small>
+                  )}
+                </div>
+              </div>
+            </div>
+            
             {/* Button */}
             <button className="btn my-btn w-100 py-2 mt-4 fw-bold">
               Create
